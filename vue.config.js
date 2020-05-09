9a31cc1a468707824fed182b0f63ef90556d9ee6
FangDingli <1191230628@qq.com>
2020-05-10 00:55:57 +0800

    complete support-icon component

---
+++ vue.config.js
@@ -0,0 +1,55 @@
+const appData=require("./data.json")
+const {seller,goods,ratings} = appData
+const path=require("path")
+
+function resolve(dir) {
+  return path.join(__dirname,dir)
+}
+
+module.exports={
+  devServer:{
+    port:3000,
+    before(app){
+      app.get("/api/seller",(request,response)=>{
+        response.json({
+          errno:0,
+          data:seller
+        })
+      }),
+      app.get("/api/goods",(request,response)=>{
+        response.json({
+          errno:0,
+          data:goods
+        })
+      })
+      app.get("/api/ratings",(request,response)=>{
+        response.json({
+          errno:0,
+          data:ratings
+        })
+      })
+    }
+  },
+
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          './src/theme'
+        ]
+      }
+    }
+  },
+
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  },
+
+  chainWebpack(config){
+    config.resolve.alias.set("components",resolve("src/components")).set("common",resolve("src/common"))
+  }
+}
