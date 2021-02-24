8481a6fdbc3381936f69309b84e240968588a69b
FangDingli <1191230628@qq.com>
2021-02-24 16:46:39 +0800

    update helper.js for nginx

--- src/api/helper.js
+++ src/api/helper.js
@@ -1,13 +1,19 @@
 import axios from 'axios'
-const ERR_OK=0
+const ERR_OK = 0
+
+const urlMap = {
+  development: '/',
+  production: 'http://8.136.112.243/'
+}
+const baseUrl = urlMap[process.env.NODE_ENV]
 
 export function get(url) {
   return function (params) {
-    return axios.get(url,{params}).then(function (res) {
-      const{errno,data}=res.data
-      if(errno===ERR_OK){
+    return axios.get(baseUrl + url, { params }).then(function (res) {
+      const { errno, data } = res.data
+      if (errno === ERR_OK) {
         return data
       }
-    }).catch(()=>{})
+    }).catch(() => { })
   }
 }
